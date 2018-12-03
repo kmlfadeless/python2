7a5d054da39da480524158bbc24bb7674609874a
Kostiantyn Lozhkin <k@kml.email>
2018-12-03 02:15:14 +0100

    DZ5 rotate logs

--- dz5/client_log_config.py
+++ dz5/client_log_config.py
@@ -1,10 +1,13 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 logger = logging.getLogger('client')
 
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(module)s %(message)s ")
 
-fh = logging.FileHandler("log/client.log", encoding='utf-8')
+fh = TimedRotatingFileHandler("log/client.log",
+                              when="midnight",
+                              backupCount=5, encoding='utf-8')
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 
--- dz5/server_log_config.py
+++ dz5/server_log_config.py
@@ -1,10 +1,13 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 serv_logger = logging.getLogger('server')
 
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(module)s %(message)s ")
 
-fh = logging.FileHandler("log/server.log", encoding='utf-8')
+fh = TimedRotatingFileHandler("log/server.log",
+                              when="midnight",
+                              backupCount=5, encoding='utf-8')
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 
